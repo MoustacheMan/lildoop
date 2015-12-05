8f46815914834518f8c599f3fce9b70ba155b418
unknown <jblodgett@student.neumont.edu>
2015-12-05 16:13:40 -0700

    Added MapReduceService into the servlet

--- src/main/java/fileStorage/concrete/MapReduceService.java
+++ src/main/java/fileStorage/concrete/MapReduceService.java
@@ -1,12 +1,22 @@
-package mapReduce.concrete;
+package fileStorage.concrete;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
 import mapReduce.interfaces.Mapper;
 import mapReduce.interfaces.Reducer;
 
+@Path("/mapReduce")
 public class MapReduceService {
 	
 	public static <K,V,T> void startMapReduce(Mapper<K,V,T> mapper, Reducer<K,V> reducer, String fileDataPath, String lilDoopAddress) {
 		
+	}
+	
+	@Path("/test")
+	@GET
+	public String test() {
+		return "test";
 	}
 
 }
