c0d4b0d42369c7e26e1c60cb0f0fe48648904ed2
David Gonzalez <dgonzalez@student.neumont.edu>
2015-12-08 19:17:24 -0700

    Started pseudo-coding out MapReduceService. Commiting current work to allow Kelvin to work individually. Random segments of code have been commented out to allow compilation

--- src/main/java/fileStorage/concrete/MapReduceService.java
+++ src/main/java/fileStorage/concrete/MapReduceService.java
@@ -1,10 +1,16 @@
 package fileStorage.concrete;
 
+import java.util.Map;
+
+import javax.print.attribute.HashDocAttributeSet;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
 import mapReduce.concrete.Dispatcher;
 
@@ -13,38 +19,91 @@ public class MapReduceService {
 	
 	private Dispatcher currentDispatcher;
 	
-	
 	@Path("/status")
 	@GET
 	public Response getStatus() {
+		//get status from dispatcher
 		return null;
 	}
 	
 	@Path("/start")
-	@Consumes("application/json")
 	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
 	public Response startMapReduce() {
-		return null;
+		//Break up JSON if need be (deserialization)
+		//Create new dispatcher
+//		this.currentDispatcher = new Dispatcher<>(mapper, reducer, data);
+		//Create response of 'accepted' or 'ok'
+		// Should include some status monitor or some time estimate (HATEOAS thing, send URL they can use to call for status)
+		ResponseBuilder responseBuilder = Response.accepted();
+		//return response
+		return responseBuilder.build();
 	}
 	
 	@Path("/getWork")
 	@POST
+	@Consumes(MediaType.WILDCARD)
 	public Response getNextWork() {
-		return null;
+		//[Have to know when were mapping or reducing here]
+		ResponseBuilder builder = Response.ok();
+		Object jobPerformerToSend = null;
+		String jobType = null;
+		int workAmount = 0;
+		Map work = null;
+		//When mapping
+		if (currentDispatcher.hasDataToMap()) {
+			//get next data set to map
+			currentDispatcher.getNextDataSetToMap(workAmount);
+			//set job type to map
+			jobPerformerToSend = currentDispatcher.getMapper();
+			jobType = "map";
+		} else {
+		//When reducing	
+			//get next mapped data set to reduce
+			currentDispatcher.getNextMappedData(workAmount);
+			//set job type to reduce
+			jobPerformerToSend = currentDispatcher.getReducer();
+			jobType = "reduce";
+		}	
+		
+		//form JSON with data and mapper or reducer according to job type
+		//create response with job type header and JSON
+		builder.header("X-JobType", jobType);
+//		builder.entity();
+		//return the response
+		return builder.build();
 	}
+	
+//	private String formWorkJson(String jobType, Map<K, V> work) {
+//		if (jobType.equals("map")) {
+//			
+//		} else if(jobType.equals("reduce")) {
+//			
+//		} else {
+//			throw new IllegalArgumentException("the specified job type is not valid");
+//		}
+//	}
 	
 	@Path("/addMapResult")
 	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response addMapResult() {
+		//Break up JSON if need be (deserialization) // They will be sending a map so this may change
+		//add map to dispatcher
+		//Create 'ok' response
+		//return response
 		return null;
 	}
 	
 	@Path("/addReduceResult")
 	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
 	public Response addReduceResult() {
+		//Break up JSON if need be (deserialization) // They will be sending a map so this may change
+		//add reduced data to dispatcher
+		//Create 'ok' response
+		//return response
 		return null;
 	}
-	
-	
-
 }
