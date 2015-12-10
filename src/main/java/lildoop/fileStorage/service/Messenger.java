8e081efd7d1999c2a18cc85ec96e3dec9e4cea0d
unknown <jblodgett@student.neumont.edu>
2015-12-09 20:52:36 -0700

    Modified Messenger method

--- src/main/java/lildoop/fileStorage/service/Messenger.java
+++ src/main/java/lildoop/fileStorage/service/Messenger.java
@@ -1,6 +1,8 @@
 package lildoop.fileStorage.service;
 
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -9,13 +11,17 @@ import lildoop.fileStorage.enums.RequestType;
 
 public class Messenger {
 	
-	public static HttpURLConnection createConnection(String url, RequestType type, String contentType)
+	public static HttpURLConnection createJsonConnection(String url, RequestType type, String json, String contentType)
 			throws MalformedURLException, IOException {
 
 		HttpURLConnection connection = null;
 		String queryString = "";
 		
 		queryString = queryString.substring(0, queryString.length()-1);
+		
+		ObjectOutputStream outputStream = new ObjectOutputStream(connection.getOutputStream());
+		outputStream.writeObject(json);
+		outputStream.flush();
 
 		connection = (HttpURLConnection) new URL(url).openConnection();
 		connection.setDoOutput(true);
